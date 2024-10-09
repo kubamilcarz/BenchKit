978aaa6713edf054eec7db51493110d9cb2a6e7f
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-10-09 10:29:19 +0200

    Initial Commit

---
+++ BenchKit/BenchKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchKitApp.swift
+//  BenchKit
+//
+//  Created by Kuba on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BenchKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
