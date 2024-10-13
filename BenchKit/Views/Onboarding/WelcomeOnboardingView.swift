5330050a386d4b443c916e9edb39ad9007e55998
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-10-13 12:50:13 +0200

    wishkit

---
+++ BenchKit/Views/Onboarding/WelcomeOnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeOnboardingview.swift
+//  BenchKit
+//
+//  Created by Kuba on 10/13/24.
+//
+
+import SwiftUI
+
+struct WelcomeOnboardingview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeOnboardingview()
+}
